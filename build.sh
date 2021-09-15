d9e634adc8eb7539561b8fbf1891ad9012dcef96
Stefan Liu <stefanliu@outlook.com>
2021-09-15 21:50:15 +0800

    Ignore proto already registered error

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-CGO_ENABLED=0 go build -tags $1 -o server .
+CGO_ENABLED=0 go build -tags $1 -ldflags "-X google.golang.org/protobuf/reflect/protoregistry.conflictPolicy=warn" -o server . 
