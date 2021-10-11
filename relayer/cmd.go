c34618e1e33f62bf3ed86a157ee4ba7c9843e079
Stefan Liu <stefanliu@outlook.com>
2021-10-11 13:20:21 +0800

    Fix status command for poly chain

--- relayer/cmd.go
+++ relayer/cmd.go
@@ -123,7 +123,7 @@ func Status(ctx *cli.Context) (err error) {
 		tx, _ := h.Height(chain, bus.KEY_HEIGHT_TX)
 		header := uint64(0)
 		switch chain {
-		case base.ONT, base.NEO, base.HEIMDALL, base.OK:
+		case base.ONT, base.NEO, base.HEIMDALL, base.OK, base.POLY:
 		default:
 			header, _ = h.poly.Node().GetSideChainHeight(chain)
 		}
