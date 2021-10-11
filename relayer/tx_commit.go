836aaf7f9e9e2bab00d734eebdf3af4437c03322
Stefan Liu <stefanliu@outlook.com>
2021-10-11 17:02:01 +0800

    Refactor delay queue

--- relayer/tx_commit.go
+++ relayer/tx_commit.go
@@ -208,8 +208,7 @@ LOOP:
 
 		// Max poll size: 100
 		if !flush || len(txs) < 100 {
-			c, _ := context.WithTimeout(ctx, time.Second)
-			tx, _ := b.TxBus.Pop(c)
+			tx, _ := b.TxBus.PopTimed(ctx, time.Second)
 			if tx != nil {
 				if tx.PolyHash == "" {
 					log.Error("Invalid poly tx, poly hash missing", "body", tx.Encode())
