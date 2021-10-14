d677c27b06491c147b2d11a07bc4aa1f0e2fec82
Stefan Liu <stefanliu@outlook.com>
2021-10-14 14:49:07 +0800

    Detect failed submit attempts for hard fork

--- msg/errors.go
+++ msg/errors.go
@@ -20,11 +20,12 @@ package msg
 import "errors"
 
 var (
-	ERR_INVALID_TX          = errors.New("Invalid TX")
-	ERR_TX_BYPASS           = errors.New("Tx bypass")
-	ERR_PROOF_UNAVAILABLE   = errors.New("Tx proof unavailable")
-	ERR_HEADER_INCONSISTENT = errors.New("Header inconsistent")
-	ERR_HEADER_MISSING      = errors.New("Header missing")
-	ERR_TX_EXEC_FAILURE     = errors.New("Tx exec failure")
-	ERR_FEE_CHECK_FAILURE   = errors.New("Tx fee check failure")
+	ERR_INVALID_TX            = errors.New("Invalid TX")
+	ERR_TX_BYPASS             = errors.New("Tx bypass")
+	ERR_PROOF_UNAVAILABLE     = errors.New("Tx proof unavailable")
+	ERR_HEADER_INCONSISTENT   = errors.New("Header inconsistent")
+	ERR_HEADER_MISSING        = errors.New("Header missing")
+	ERR_TX_EXEC_FAILURE       = errors.New("Tx exec failure")
+	ERR_FEE_CHECK_FAILURE     = errors.New("Tx fee check failure")
+	ERR_HEADER_SUBMIT_FAILURE = errors.New("Header submit failure")
 )
