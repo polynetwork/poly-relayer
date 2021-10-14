2ce45bb6a73c17e42ccb1f679794963b39e99c38
Stefan Liu <stefanliu@outlook.com>
2021-10-14 16:42:30 +0800

    Detect always failing transaction

--- msg/errors.go
+++ msg/errors.go
@@ -28,4 +28,5 @@ var (
 	ERR_TX_EXEC_FAILURE       = errors.New("Tx exec failure")
 	ERR_FEE_CHECK_FAILURE     = errors.New("Tx fee check failure")
 	ERR_HEADER_SUBMIT_FAILURE = errors.New("Header submit failure")
+	ERR_TX_EXEC_ALWAYS_FAIL   = errors.New("Tx exec always fail")
 )
