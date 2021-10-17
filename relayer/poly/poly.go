d8552cd13366fa41daabee09a97b303a8d52c231
Stefan Liu <stefanliu@outlook.com>
2021-10-17 18:41:40 +0800

    Reduce poly confirm check attempts

--- relayer/poly/poly.go
+++ relayer/poly/poly.go
@@ -181,7 +181,7 @@ func (s *Submitter) SubmitHeaders(chainId uint64, headers [][]byte) (hash string
 		return "", err
 	}
 	hash = tx.ToHexString()
-	_, err = s.sdk.Node().Confirm(hash, 0, 80)
+	_, err = s.sdk.Node().Confirm(hash, 0, 30)
 	if err == nil {
 		log.Info("Submitted header to poly", "chain", chainId, "hash", hash)
 	}
