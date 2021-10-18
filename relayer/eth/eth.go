7fd2d55206ab1f0c15c7dbf95fe305c46afb6ee0
Stefan Liu <stefanliu@outlook.com>
2021-10-18 21:34:21 +0800

    No more retry after a verify success

--- relayer/eth/eth.go
+++ relayer/eth/eth.go
@@ -266,7 +266,7 @@ func (s *Submitter) run(account accounts.Account, mq bus.TxBus, delay bus.Delaye
 			case base.BSC, base.HECO, base.OK:
 				tsp = time.Now().Unix() + 60*4
 			}
-			if tsp > 0 {
+			if tsp > 0 && tx.DstHash != "" {
 				bus.SafeCall(s.Context, tx, "push to delay queue", func() error { return delay.Delay(context.Background(), tx, tsp) })
 			}
 		}
