22b851d6b3899d6d137bbaf70c1f4f6f408a042f
Stefan Liu <stefanliu@outlook.com>
2021-10-27 15:15:45 +0800

    Increase attempt for failed fee check tx

--- relayer/tx_commit.go
+++ relayer/tx_commit.go
@@ -180,11 +180,13 @@ func (b *CommitFilter) flush(ctx context.Context, txs []*msg.Tx) (err error) {
 		} else if check.Skip() {
 			log.Warn("Skipping poly for marked as not target in fee check", "poly_hash", tx.PolyHash)
 		} else if check.Missing() {
+			tx.Attempts++
 			log.Info("CheckFee tx missing in bridge, delay for 2 seconds", "poly_hash", tx.PolyHash)
 			tsp := time.Now().Unix() + 5
 			bus.SafeCall(ctx, tx, "push to delay queue", func() error { return b.delay.Delay(context.Background(), tx, tsp) })
 
 		} else {
+			tx.Attempts++
 			log.Info("CheckFee tx not paid, delay for 10 minutes", "poly_hash", tx.PolyHash, "min", feeMin, "paid", feePaid)
 			tsp := time.Now().Unix() + 600
 			bus.SafeCall(ctx, tx, "push to delay queue", func() error { return b.delay.Delay(context.Background(), tx, tsp) })
