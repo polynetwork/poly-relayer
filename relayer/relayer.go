0550ddd24a5c2acdb8f25d907b44d2bb70027d07
rain-zxn <206131521@qq.com>
2022-05-11 22:42:41 +0800

    delete http SubmitterConfig.Wallet

--- relayer/relayer.go
+++ relayer/relayer.go
@@ -147,6 +147,7 @@ func DstSubmitter(chain uint64) (sub IChainSubmitter, err error) {
 	if conf == nil || conf.PolyTxCommit == nil {
 		return nil, fmt.Errorf("No config available for submitter of chain %d", chain)
 	}
+	conf.PolyTxCommit.SubmitterConfig.Wallet = nil
 	err = sub.Init(conf.PolyTxCommit.SubmitterConfig)
 	return
 }
