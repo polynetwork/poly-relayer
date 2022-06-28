8832bc829dddd21b9e1699603120119357ecfe7a
wuyachi <wuyachi@163.com>
2022-06-28 18:43:18 +0800

    update

--- relayer/relayer.go
+++ relayer/relayer.go
@@ -40,7 +40,6 @@ import (
 	"github.com/polynetwork/poly-relayer/relayer/ok"
 	"github.com/polynetwork/poly-relayer/relayer/ont"
 	po "github.com/polynetwork/poly-relayer/relayer/poly"
-	"github.com/polynetwork/poly-relayer/relayer/starcoin"
 )
 
 type IChainListener interface {
